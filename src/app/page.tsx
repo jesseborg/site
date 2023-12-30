75309b931c23abf3427a43a8c22fb2be86ca455c
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-12-30 22:04:12 +1000

    add: basic header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,58 @@
 'use client';
 
 import { Background } from '@/components/background/background';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<main className="flex h-[100svh] items-center justify-center">
+		<main className="flex h-[100svh] justify-center">
 			<Background />
-			<h1 className="relative text-4xl font-bold text-white ">Hello World!</h1>
+
+			<div className="flex w-full ">
+				<Fade />
+				<header className="flex w-[640px] flex-col gap-3 px-6 pt-8 font-mono text-xs sm:pt-16">
+					<div className="flex items-center">
+						<div className="flex items-center">
+							<span aria-hidden className="ping" />
+							<p>Available for work</p>
+						</div>
+						<hr
+							aria-hidden
+							className="mx-3 flex-1 self-center border-neutral-600 mix-blend-lighten"
+						/>
+						<p>9:15:00 PM</p>
+					</div>
+
+					<div className="relative">
+						<Image
+							src="/images/header.png"
+							alt="Monochromatic image of a bunch of leaves with a halftone effect applied"
+							width={640}
+							height={150}
+						/>
+						<Image
+							draggable={false}
+							className="rendering-pixelated relative mx-auto -translate-y-1/2"
+							src="/images/name.png"
+							alt="My name gradually getting more pixelated with each letter"
+							width={572}
+							height={108}
+						/>
+					</div>
+				</header>
+				<Fade reverse />
+			</div>
 		</main>
+	);
+}
+
+function Fade({ reverse = false }: { reverse?: boolean }) {
+	return (
+		<span
+			className={
+				'hidden h-full w-20 flex-1 from-black to-transparent sm:block' +
+				(reverse ? ' bg-gradient-to-l' : ' bg-gradient-to-r')
+			}
+		/>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
 		extend: {
 			keyframes: {
-				"background-pulse": {
-					from: { opacity: "0.75" },
-					to: { opacity: "0.8" },
+				'background-pulse': {
+					from: { opacity: '0.75' },
+					to: { opacity: '0.8' }
 				},
-				"background-spin": {
-					to: { transform: "rotate(360deg)" },
+				'background-spin': {
+					to: { transform: 'rotate(360deg)' }
 				},
+				'ping': {
+					'75%, 100%': {
+						transform: 'scale(var(--ping-scale, 2))',
+						opacity: '0'
+					}
+				}
 			},
 			animation: {
 				background:
-					"background-pulse 6s ease-in-out infinite alternate, background-spin 120s linear infinite",
-			},
-		},
+					'background-pulse 6s ease-in-out infinite alternate, background-spin 120s linear infinite',
+				ping: 'ping var(--ping-duration, 1s) cubic-bezier(0, 0, 0.2, 1) infinite'
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 };
 export default config;
