185798003890a6014dd16b0bf167f1a581d194f4
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-12-31 22:26:32 +1000

    add: basic home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,97 @@
 'use client';
 
 import { Background } from '@/components/background/background';
+import { SuitcaseIcon } from '@/components/icons/suitcase-icon';
+import { TerminalIcon } from '@/components/icons/terminal-icon';
+import { Section } from '@/components/section';
 import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<main className="flex h-[100svh] justify-center">
+		<div className="flex h-[100svh] justify-center">
 			<Background />
 
-			<div className="flex w-full ">
+			<div className="flex w-full">
 				<Fade />
-				<header className="flex w-[640px] flex-col gap-3 px-6 pt-8 font-mono text-xs sm:pt-16">
-					<div className="flex items-center">
+				<div className="w-[640px] px-6 pt-8 sm:pt-16 md:px-0">
+					<header className="flex flex-col gap-3 font-mono text-xs">
 						<div className="flex items-center">
-							<span aria-hidden className="ping" />
-							<p>Available for work</p>
+							<div className="flex items-center">
+								<span aria-hidden className="ping" />
+								<p className="flex items-center gap-px">Available for work</p>
+							</div>
+							<hr
+								aria-hidden
+								className="mx-3 flex-1 self-center border-neutral-600 mix-blend-lighten"
+							/>
+							<p>9:15:00 PM</p>
 						</div>
-						<hr
-							aria-hidden
-							className="mx-3 flex-1 self-center border-neutral-600 mix-blend-lighten"
-						/>
-						<p>9:15:00 PM</p>
-					</div>
+						<div className="relative">
+							<Image
+								src="/images/header.png"
+								alt="Monochromatic image of a bunch of leaves with a halftone effect applied"
+								width={640}
+								height={150}
+							/>
+							<Image
+								draggable={false}
+								className="rendering-pixelated relative mx-auto w-[89.5%] -translate-y-1/2"
+								src="/images/name.png"
+								alt="My name gradually getting more pixelated with each letter"
+								width={572}
+								height={108}
+							/>
+						</div>
+					</header>
 
-					<div className="relative">
-						<Image
-							src="/images/header.png"
-							alt="Monochromatic image of a bunch of leaves with a halftone effect applied"
-							width={640}
-							height={150}
-						/>
-						<Image
-							draggable={false}
-							className="rendering-pixelated relative mx-auto -translate-y-1/2"
-							src="/images/name.png"
-							alt="My name gradually getting more pixelated with each letter"
-							width={572}
-							height={108}
-						/>
-					</div>
-				</header>
+					<main className="flex flex-col gap-8 text-base">
+						<div className="text-sm leading-6 -tracking-[0.2px] text-neutral-100 [&_span]:text-neutral-400">
+							<p>
+								I&apos;m currently a hobbyist <span>web developer</span> looking for work in{' '}
+								<span>front-end development</span>, with a growing interest in back-end and software
+								development.
+							</p>
+							<br />
+							<p>
+								Recently I&apos;ve been learning <span>Rust</span> in my free time to create desktop
+								apps with the <span>Tauri</span> framework.
+							</p>
+						</div>
+
+						<Section.Root>
+							<Section.Header>
+								<Section.Icon>
+									<SuitcaseIcon />
+								</Section.Icon>
+								<Section.Title>projects</Section.Title>
+								<Section.Link href="/projects">see more -&gt;</Section.Link>
+							</Section.Header>
+
+							<Section.Body className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+								<h1>Hello World</h1>
+								<h1>Hello World</h1>
+								<h1>Hello World</h1>
+								<h1>Hello World</h1>
+							</Section.Body>
+						</Section.Root>
+
+						<Section.Root>
+							<Section.Header>
+								<Section.Icon>
+									<TerminalIcon />
+								</Section.Icon>
+								<Section.Title>stack</Section.Title>
+							</Section.Header>
+
+							<Section.Body>
+								<h1>Hello World</h1>
+							</Section.Body>
+						</Section.Root>
+					</main>
+				</div>
 				<Fade reverse />
 			</div>
-		</main>
+		</div>
 	);
 }
 
---
+++ src/components/section.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { AnchorHTMLAttributes, HTMLAttributes, PropsWithChildren } from 'react';
+
+function Root({ children }: PropsWithChildren<{}>) {
+	return <section className="flex flex-col gap-4">{children}</section>;
+}
+
+function Title({ children }: PropsWithChildren<{}>) {
+	return <p className="flex-1 text-base font-bold italic text-neutral-50">{children}</p>;
+}
+
+function Icon({ children }: PropsWithChildren<{}>) {
+	return <span>{children}</span>;
+}
+
+function Link({ children, ...props }: PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>) {
+	return (
+		<a className="text-xs font-light italic" {...props}>
+			{children}
+		</a>
+	);
+}
+
+function Header({ children }: PropsWithChildren<{}>) {
+	return <div className="flex items-center gap-2">{children}</div>;
+}
+
+function Body({ children, ...props }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+	return <div {...props}>{children}</div>;
+}
+
+export const Section = {
+	Root,
+	Title,
+	Icon,
+	Link,
+	Header,
+	Body
+};
