15b8770bf4a5cb3a171198e75daf350e0f90d9ec
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-11 20:45:15 +1000

    style: rotate badge arrow on hover/focus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 				</div>
 			</div>
 			<div className="flex flex-col gap-8 text-base">
-				<div className="prose leading-6 -tracking-[0.2px] text-neutral-300 dark:prose-invert [&_span]:text-neutral-400">
+				<div className="prose leading-6 -tracking-[0.2px] text-neutral-300 dark:prose-invert [&>span]:text-neutral-400">
 					<p>
 						I&apos;m a hobbyist web developer looking for work in front-end development, with a
 						growing interest in back-end and software development.
@@ -119,10 +119,12 @@ function Badge({ href, children }: { href: string; children: ReactNode }) {
 		<Link
 			target="_blank"
 			href={href}
-			className="not-prose inline-flex items-baseline gap-1 rounded-md border border-neutral-700 bg-neutral-800 px-1.5 py-1 text-sm text-white outline-offset-0 transition-colors hover:border-neutral-600 hover:bg-neutral-700 focus-visible:border-neutral-600 focus-visible:bg-neutral-700 [&>svg]:self-center"
+			className="not-prose group inline-flex items-baseline gap-1 rounded-md border border-neutral-700 bg-neutral-800 px-1.5 py-1 text-sm text-white outline-offset-0 transition-colors hover:border-neutral-600 hover:bg-neutral-700 focus-visible:border-neutral-600 focus-visible:bg-neutral-700 [&>svg]:self-center"
 		>
 			{children}
-			<span className="icon-north-east-arrow self-start text-xs [&::before]:text-neutral-300" />
+			<span className="relative -top-0.5 origin-center font-sans-display text-xs text-neutral-300 transition-transform group-hover:-rotate-12 group-focus-visible:-rotate-12">
+				&#x2197;
+			</span>
 		</Link>
 	);
 }
