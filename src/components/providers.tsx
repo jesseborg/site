2462db722559e0f0d4c7dcad72aa9525ad5dbcee
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-11 18:26:13 +1000

    fix: remove semicolon

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from 'react';
 export function Providers({ children }: { children: ReactNode }) {
 	return (
 		<>
-			<TooltipProvider>{children}</TooltipProvider>;
+			<TooltipProvider>{children}</TooltipProvider>
 		</>
 	);
 }
