0693dda8566cd9e93a9984621705cd65eba35f1b
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-11 18:28:28 +1000

    style: change projects nav style on focus

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -4,7 +4,10 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<div className="relative mx-auto grid max-w-2xl gap-6 lg:max-w-none lg:grid-cols-[1fr_672px_1fr]">
 			<nav className="left-0 top-12 mb-0 mr-6 h-fit pt-2 sm:sticky">
-				<Link className="group flex text-sm italic text-neutral-200 hover:text-white" href="/">
+				<Link
+					className="group flex rounded-sm text-sm italic text-neutral-200 hover:text-white focus-visible:text-white"
+					href="/"
+				>
 					<span className="icon-right-hook-arrow mr-2 inline-block rotate-180 pt-px" />
 					<span className="group-hover:underline">Home</span>
 				</Link>
