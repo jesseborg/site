7bd971a81972137ab3dfabf6c15db315cc6e49d4
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-11 23:15:55 +1000

    style: fix /projects layout max width

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="relative mx-auto grid max-w-2xl gap-6 lg:max-w-none lg:grid-cols-[1fr_672px_1fr]">
+		<div className="relative mx-auto grid max-w-2xl gap-6 lg:max-w-fit lg:grid-cols-[120px_672px_120px]">
 			<nav className="left-0 top-12 mb-0 mr-6 h-fit pt-2 sm:sticky">
 				<Link
 					className="group flex rounded-sm text-sm italic text-neutral-200 hover:text-white focus-visible:text-white"
