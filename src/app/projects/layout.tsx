46e660422346406ebcbce1f0dca1bb56955be2bb
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-22 01:07:33 +1000

    fix: sticky nav only on 'lg' screens

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -23,7 +23,7 @@ export default function PageLayout({ children }: { children: React.ReactNode }) 
 
 	return (
 		<div className="relative mx-auto grid max-w-2xl gap-6 lg:max-w-fit lg:grid-cols-[120px_672px_120px]">
-			<nav className="left-0 top-12 mb-0 mr-6 h-fit pt-2 sm:sticky">
+			<nav className="left-0 top-12 mb-0 mr-6 h-fit pt-2 lg:sticky">
 				<Link
 					className="group flex rounded-sm text-sm italic text-neutral-200 hover:text-white focus-visible:text-white"
 					href={path.href}
