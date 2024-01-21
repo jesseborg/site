28e59b75b636570e6b97c555f1566b14770094af
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-01-21 17:32:57 +1000

    change: replace lorem ipsum with real info for image cropper page

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,7 +1,19 @@
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
 import Link from 'next/link';
+import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 import { Badge } from './badge';
 import { SpotifyWidget, SpotifyWidgetContainer } from './spotify-widget/spotify-widget';
+
+function Anchor(
+	props: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+) {
+	return (
+		<a
+			className="border-b-2 border-neutral-100 text-neutral-200 no-underline hover:text-neutral-50"
+			{...props}
+		/>
+	);
+}
 
 export function MDXComponent(props: MDXRemoteProps) {
 	return (
@@ -10,7 +22,8 @@ export function MDXComponent(props: MDXRemoteProps) {
 				Badge,
 				Link,
 				SpotifyWidget,
-				SpotifyWidgetContainer
+				SpotifyWidgetContainer,
+				a: Anchor
 			}}
 			{...props}
 		/>
