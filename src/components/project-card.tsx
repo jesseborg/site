85a0ec0313df81c239f0b2e97f3d4025bf5833a6
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-15 20:56:08 +1000

    style: use 'ring' instead of 'border' on `ProjectCard`
    this helps to avoid layout shift with the border property

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,4 +1,4 @@
-import { dynamicBlurDataURL } from '@/lib/util';
+import { cn, dynamicBlurDataURL } from '@/lib/util';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -14,7 +14,11 @@ export async function ProjectCard(props: ProjectCardProps) {
 
 	return (
 		<Link
-			className="group min-w-[332px] rounded-xl border border-theme-50/30 bg-theme-900 p-1 transition-colors duration-200 hover:border-theme-50/40 focus-visible:border-2 focus-visible:border-theme-300 focus-visible:outline-0"
+			className={cn(
+				'group min-w-[332px] rounded-xl border border-theme-50/30 bg-theme-900 p-1 transition-colors duration-200',
+				'hover:border-theme-50/40',
+				'focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-theme-300'
+			)}
 			href={props.href}
 			rel="noreferrer"
 		>
@@ -35,14 +39,13 @@ export async function ProjectCard(props: ProjectCardProps) {
 				</span>
 
 				<div
-					className={
-						'absolute bottom-0 left-0 z-10 w-full p-4 font-sans-display duration-200 ease-in-out will-change-transform group-hover:-translate-y-[2px]'
-					}
+					className={cn(
+						'absolute bottom-0 left-0 z-10 w-full p-4 font-sans-display text-sm leading-4 duration-200 ease-in-out will-change-transform',
+						'group-focus-within:-translate-y-[2px] group-hover:-translate-y-[2px]'
+					)}
 				>
-					<p className="text-sm font-medium leading-4 text-white sm:leading-6">{props.title}</p>
-					<p className="text-sm font-normal leading-4 text-neutral-300 sm:leading-[9px]">
-						{props.description}
-					</p>
+					<p className="font-medium text-white sm:leading-6">{props.title}</p>
+					<p className="font-normal text-neutral-300 sm:leading-[9px]">{props.description}</p>
 				</div>
 			</div>
 		</Link>
